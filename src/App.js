78b749bebe32a5d5ef4285f93cfad48681ec3f85
Fernando Horas <fernando.horas@fchdigital.com.br>
2022-10-17 16:17:17 -0300

    Update v1

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ const App = () => {
 
   return (
     <div>
-      <h1 className="title">Vamos aprender sobre DevSecOps</h1>
+      <h1 className="title">Vamos aprender sobre DevOps e DevSecOps</h1>
 
       <SearchForm
         searchTerm={searchTerm}
